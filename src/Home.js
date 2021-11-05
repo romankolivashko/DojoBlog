a3212fe3663a62d3160ab1431857f3bd6e23d76d
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-05 12:24:01 -0700

    added state hook

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,26 @@
-const Home = () => {
+import { useState } from "react";
 
-  const handleClick = (e) => {
-    console.log('hello, ninjas');
+const Home = () => {
+  //let name = 'mario';
+  const [name, setName] = useState('mario');
+
+  const handleClick = () => {
+    setName('luigi');
   }
 
   const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e);
+    console.log('hello ' + name, e.target);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <button onClick = { handleClick }>Click Me</button>
+      <p>{ name }</p>
+      <button onClick={handleClick}>Click Me</button>
+      {/* <button onClick = { handleClick }>Click Me</button>
       <button onClick = { (e) => {
         handleClickAgain('mario', e);
-      }}> Click me again </button>
+      }}> Click me again </button> */}
 
     </div>
     );
