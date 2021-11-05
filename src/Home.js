a943fff19ada4889b3d00e9bece0925a4ca557f3
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-05 12:47:17 -0700

    Added list output, plus styling

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,23 @@
 import { useState } from "react";
 
 const Home = () => {
-  //let name = 'mario';
-  const [name, setName] = useState('mario');
+ 
 
-  const handleClick = () => {
-    setName('luigi');
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-  }
-
-  return (
-    <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name }</p>
-      <button onClick={handleClick}>Click Me</button>
-      {/* <button onClick = { handleClick }>Click Me</button>
-      <button onClick = { (e) => {
-        handleClickAgain('mario', e);
-      }}> Click me again </button> */}
-
-    </div>
+    const [blogs, setBlogs] = useState([
+      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+    
+    return (
+      <div className="home">
+        {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id} >
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </div>
+        ))}
+      </div>
     );
 }
  
