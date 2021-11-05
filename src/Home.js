12ea9bb4915c78105b71ceff6150b871a20b452d
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-05 12:14:01 -0700

    Added event handlers

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,21 @@
 const Home = () => {
 
-  const handleClick = () => {
+  const handleClick = (e) => {
     console.log('hello, ninjas');
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick = { handleClick }>Click Me</button>
+      <button onClick = { (e) => {
+        handleClickAgain('mario', e);
+      }}> Click me again </button>
+
     </div>
     );
 }
