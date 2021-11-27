9b458c0721e7d717f042a5759865a20b693ea66a
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-27 11:23:50 -0800

    Added err catch logic

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = ({ blogs, title }) => {
       <h2>{ title }</h2>
       {blogs.map((blog) => 
           <div className="blog-preview">
-            <h2> blog.title </h2>
+            <h2> {blog.title} </h2>
             <p> Written by: {blog.author} </p>
           </div>
         )}
--- src/Home.js
+++ src/Home.js
@@ -10,21 +10,34 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState('mario');
+    const [isPending, setIsPeding] = useState(true);
+    const [error, setError] = useState(null);
    
     useEffect(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          console.log(res);
+          if(!res.ok){
+            throw Error('could not fetch the data for that resource');
+          }
           return res.json();
         })
         .then(data => {
           setBlogs(data)
+          setIsPeding(false);
+          setError(null);
+        })
+        .catch(err => {
+          // console.log(err.message);
+          setIsPeding(false);
+          setError(err.message)
         })
     }, []);
 
     return (
       <div className="home">
+        { error && <div>{ error }</div> }
+        { isPending && <div>Loading...</div> }
         { blogs && <BlogList blogs = {blogs} title = "All Blogs" /> }
       </div>
     );
